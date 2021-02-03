fd61d6cd3256c0f1ce40b729215ead5c173d24fa
elsiempk <elsiepk@gmail.com>
2021-02-03 13:49:15 -0500

    Basket with Mouse
    - Basket script to get the basket moving whenever the mouse does.
    - make sure basket script is attached to basket prefab in project pane

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -13,6 +13,18 @@ public class Basket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Get screen position of the mouse 
+        Vector3 mousePos2D = Input.mousePosition;
+
+        // Get camera position - sees how far to push the mouse
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        // Convert the point from 2D screen space into 3D game space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        // Move x position of the basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
     }
 }
