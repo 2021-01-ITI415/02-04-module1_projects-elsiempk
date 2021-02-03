0b96068669aad66832b34ed971e75a709bcd4b0a
elsiempk <elsiepk@gmail.com>
2021-02-03 00:41:19 -0500

    AppleTree Script
    - script for apple tree
    - make sure to add the apple prefab or else error

---
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+
+    // Prefab for instantiating apples
+    public GameObject applePrefab;
+
+    // Speed at which the AppleTree moves
+    public float speed = 1f;
+
+    // Distance where AppleTree turns around
+    public float leftAndRightEdge = 20f;
+
+    // Chance that the AppleTree will change di
+    public float chanceToChangeDirections = 0.1f;
+
+    // Rate at which Apple will be instantiated
+    public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Dropping apples every second
+        InvokeRepeating("DropApple", 2f, secondsBetweenAppleDrops);
+    }
+    void DropApple()
+    {
+        GameObject apple = Instantiate(applePrefab) as GameObject;
+        apple.transform.position = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // Movements and direction changes
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+        if(pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); // Moves right
+        } else if(pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); // Moves left
+        } 
+    }
+    private void FixedUpdate()
+    {
+        // Changes Directions Randomly
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; // Changes Directions
+        }
+    }
+}
