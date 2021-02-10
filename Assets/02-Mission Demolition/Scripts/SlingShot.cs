195408bad24780007e1935fb1286f7adf813dbf0
elsiempk <elsiepk@gmail.com>
2021-02-10 11:38:04 -0500

    slingshot class
    - c of slingshot
    - launch point component added
    - changed script

---
+++ Assets/02-Mission Demolition/Scripts/SlingShot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShot : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void OnMouseEnter()
+    {
+        print("SlingShot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void OnMouseExit()
+    {
+        print("SlingShot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+}
