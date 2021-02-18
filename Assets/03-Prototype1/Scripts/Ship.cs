3af599c0540a34e4f30c033e4a33a298a08f66db
elsiempk <elsiepk@gmail.com>
2021-02-18 13:59:55 -0500

    ship script
    - script for ship
    - attach to ship
    - missile prefab attached

---
+++ Assets/03-Prototype1/Scripts/Ship.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    // prefab for instantiating apples
+    public GameObject MissilePrefab;
+
+    // speed at which the ship moves
+    public float speed = 1f;
+
+    // distance where ship turns around
+    public float chanceToChangeDirections = 0.1f;
+
+    // rate at which missiles will be instantiated
+    public float secondsBetweenMissileDrops = 1f;
+
+    void Start()
+    {
+        // dropping missiles every second
+    }
+
+    void Update()
+    {
+        // basic movement
+        // changing direction
+    }
+}
