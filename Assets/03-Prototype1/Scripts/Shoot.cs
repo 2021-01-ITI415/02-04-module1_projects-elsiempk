df31aaa48a4106ec975b060522c52b7fa516a68d
elsiempk <elsiepk@gmail.com>
2021-02-18 17:19:31 -0500

    launch
    - launch becomes child of player
    - halo around launch not player
    - launchpoint c

---
+++ Assets/03-Prototype1/Scripts/Shoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+  public GameObject playerLaunch;
+    
+  void Awake()
+    {
+        Transform playerLaunchTrans = transform.Find("PlayerLaunch");
+        playerLaunch = playerLaunchTrans.gameObject;
+        playerLaunch.SetActive(false);
+    }
+
+  void OnMouseEnter()
+    {
+        print("Shoot:OnMouseEnter()");
+        playerLaunch.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        print("Shoot:OnMouseExit()");
+        playerLaunch.SetActive(false);
+    }
+}
